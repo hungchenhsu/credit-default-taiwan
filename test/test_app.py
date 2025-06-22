a2f784ad1838b62bde042c39b39836e83a1727b1
Hung-Chen Hsu <hc.hsu777@gmail.com>
2025-06-22 14:38:28 -0400

    Update test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -4,12 +4,24 @@ from api.app import app
 
 client = TestClient(app)
 
-def test_root():
-    r = client.get("/")
-    assert r.status_code == 200
-    assert "live" in r.json().get("msg", "")
-
-def test_predict_empty():
-    r = client.post("/predict", json={"records":[]})
-    assert r.status_code == 200
-    assert r.json() == []
+def test_predict_ok():
+    response = client.post("/predict", json={
+        "records": [{
+            "LIMIT_BAL": 200000,
+            "SEX": 2,
+            "EDUCATION": 2,
+            "MARRIAGE": 1,
+            "AGE": 29,
+            "PAY_0": 1, "PAY_2": 0, "PAY_3": 0,
+            "PAY_4": 0, "PAY_5": 0, "PAY_6": 0,
+            "BILL_AMT1": 3913, "BILL_AMT2": 3102, "BILL_AMT3": 689,
+            "BILL_AMT4": 0, "BILL_AMT5": 0, "BILL_AMT6": 0,
+            "PAY_AMT1": 0, "PAY_AMT2": 1000, "PAY_AMT3": 1000,
+            "PAY_AMT4": 1000, "PAY_AMT5": 0, "PAY_AMT6": 0
+        }]
+    })
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+    assert "probability" in data[0]
+    assert "label" in data[0]
